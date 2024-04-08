b5e34b396fe4bf43cf6ae04b4a48e43c72f7f992
timothy ademola <timife007@gmail.com>
2024-04-08 11:49:16 +0100

    services archis and app classes

---
+++ content-management/src/main/java/com/timife/ContentManApplication.java
@@ -0,0 +1,11 @@
+package com.timife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentManApplication.class, args);
+    }
+}
