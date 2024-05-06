b6b1a7fc2f3d22c0b3add617864ef7d3e885c384
timothy ademola <timife007@gmail.com>
2024-05-06 12:50:18 +0100

    impl auth discovery  client

--- authentication/src/main/java/com/timife/AuthApplication.java
+++ authentication/src/main/java/com/timife/AuthApplication.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
     public static final Logger logger = LoggerFactory.getLogger(AuthApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
