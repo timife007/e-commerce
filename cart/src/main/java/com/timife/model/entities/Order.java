07add2621bd5f8c214fda7006ec36cd4c0d2e18c
timothy ademola <timife007@gmail.com>
2024-07-12 16:40:47 +0100

    impl broker

--- cart/src/main/java/com/timife/model/entities/Order.java
+++ cart/src/main/java/com/timife/model/entities/Order.java
@@ -18,6 +18,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
     private Cart cart;
     private OrderStatus orderStatus;
 }
--- gateway-service/src/main/java/com/timife/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/timife/GatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.timife;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableDiscoveryClient
