e94ed068bee2d7849ef63bbd799aba4dea7148f2
timothy ademola <timife007@gmail.com>
2024-07-26 13:27:47 +0100

    send message with attachment method

--- payment/src/main/java/com/timife/services/impl/PaymentServiceImpl.java
+++ payment/src/main/java/com/timife/services/impl/PaymentServiceImpl.java
@@ -7,12 +7,18 @@ import com.timife.model.responses.UserResponse;
 import com.timife.repositories.PaymentRepository;
 import com.timife.services.PaymentStatusPublishService;
 import com.timife.services.PaymentService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 @RequiredArgsConstructor
@@ -74,6 +80,24 @@ public class PaymentServiceImpl implements PaymentService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+        javaMailSender.send(message);
+    }
+
+    private void sendMessageWithAttachment(
+            String to, String subject, String text) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("noreply@baeldung.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+
+        FileSystemResource file
+                = new FileSystemResource(new File("pathToAttachment"));
+        helper.addAttachment("Invoice", file);
+
         javaMailSender.send(message);
     }
 }
